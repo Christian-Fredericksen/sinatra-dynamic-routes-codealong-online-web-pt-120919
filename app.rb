f31440151026f20109cd4cea610aba71fff44be8
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-03-18 17:41:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -23,6 +23,6 @@ class App < Sinatra::Base
   get "/multiply/:num1/:num2" do
     @num1 = params[:num1].to_i
     @num2 = params[:num2].to_i
-    "The answer is #{@answer}!"
+    "The answer is #{:num1 * :num2!}"
   end
 end
