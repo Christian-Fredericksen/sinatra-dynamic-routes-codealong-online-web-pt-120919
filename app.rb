b7f9930f26c4011e5f6bcd04db5f675216352e34
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-03-18 17:38:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ class App < Sinatra::Base
     "Goodbye #{@user_name}!"
   end
   
-  get "/multiply" do
+  get "/multiply/:num1/:num2" do
     answer = params[:num1, :num2]
     "The answer is #{@answer}!"
   end
